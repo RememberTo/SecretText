8f18a5d796c87a506725fdba3aa1d033910df4ff
Кирилл <69306571+RememberTo@users.noreply.github.com>
2021-02-28 23:11:29 +0300

    first commit

---
+++ SecretText/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+
+namespace SecretText
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Label_code.Visibility = Visibility.Hidden;
+            Label_Uncode.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // открыть файл
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                EnterCode.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) // сохранить в файл
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, ResultCode.Text);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TextBoxKey.Text == "")
+                {
+                    MessageBox.Show("Введите ключ шифровки");
+                    return;
+                }
+
+                if (EnterCode.Text == "")
+                {
+                    if (RadioButton_Encoding.IsChecked == true)
+                    {
+                        MessageBox.Show("Нечего шифровать!!!\nЗаполните поле с текстом");
+                    }
+                    if (RadioButton_Decoding.IsChecked == true)
+                    {
+                        MessageBox.Show("Нечего расшифровывать!!!\nЗаполните поле с текстом");
+                    }
+                }
+
+                Int64 per = Convert.ToInt32(TextBoxKey.Text);
+
+                if (RadioButton_Encoding.IsChecked == false && RadioButton_Decoding.IsChecked == false)
+                {
+                    MessageBox.Show("Выберете зашифровать или расшифровать информацию");
+                    return;
+                }
+                if (RadioButton_Encoding.IsChecked == true)
+                {
+                    Label_Uncode.Visibility = Visibility.Hidden;
+                    Label_code.Visibility = Visibility.Visible;
+                    Pressmark pressmark = new Pressmark(TextBoxKey.Text);
+                    string result = pressmark.Encoding(EnterCode.Text);
+                    ResultCode.Text = result;
+                }
+                if (RadioButton_Decoding.IsChecked == true)
+                {
+                    Label_code.Visibility = Visibility.Hidden;
+                    Label_Uncode.Visibility = Visibility.Visible;
+                    Pressmark pressmark = new Pressmark(TextBoxKey.Text);
+                    string result = pressmark.Decoding(EnterCode.Text);
+                    ResultCode.Text = result;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ключ должен состоять только из чисел");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Слишком большой ключ");
+            }
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Label_code.Visibility = Visibility.Hidden;
+            Label_Uncode.Visibility = Visibility.Hidden;
+            ResultCode.Text = EnterCode.Text;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Label_code.Visibility = Visibility.Hidden;
+            Label_Uncode.Visibility = Visibility.Hidden;
+            EnterCode.Text = ResultCode.Text;
+        }
+    }
+}
---
+++ SecretText/Pressmark.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretText
+{
+    class Pressmark
+    {
+        private string Key { get; set; }
+        private string PromptKey { get; set; }
+        public string abc = "abcdefghijklmnopqrstuvwxyz";
+        public string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string abc_ru = "абвгдеёжзийклмнопрстуфхцчшщъьэюя";
+        public string ABC_ru = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЭЮЯ";
+        public string num = "0123456789";
+
+
+        public Pressmark(string key)
+        {
+            Key = key;
+        }
+
+        public string Encoding(string text)
+        {
+            string result = "";
+            PromptKey = Key;
+            while (PromptKey.Length < text.Length)
+            {
+                PromptKey += Key;
+            }
+            if (PromptKey.Length > text.Length)
+            {
+                int lenTxt = PromptKey.Length - text.Length;
+                for (int i = 0; i < lenTxt; i++)
+                {
+                    PromptKey += Key[i];
+                }
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                int indexof_a = abc.IndexOf(text[i]);
+                int indexof_A = ABC.IndexOf(text[i]);
+                int indexof_a_ru = abc_ru.IndexOf(text[i]);
+                int indexof_A_ru = ABC_ru.IndexOf(text[i]);
+                int indexof_num = num.IndexOf(text[i]);
+                if (indexof_num != -1)
+                {
+                    int offset = num.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48);
+                    if (offset >= num.Length)
+                        offset = offset - num.Length;
+                    else if (offset < 0)
+                        offset = num.Length + offset;
+                    result += num[offset];
+                }
+                if (indexof_a != -1)
+                {
+                    int offset = abc.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48); // вычитаем 48 для того чтобы перевести в число
+                    if (offset >= abc.Length)
+                        offset = offset - abc.Length;
+                    else if (offset < 0)
+                        offset = abc.Length + offset;
+                    result += abc[offset];
+                }
+                if (indexof_A != -1)
+                {
+                    int offset = ABC.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48);
+                    if (offset >= ABC.Length)
+                        offset = offset - ABC.Length;
+                    else if (offset < 0)
+                        offset = ABC.Length + offset;
+                    result += ABC[offset];
+                }
+                if (indexof_a_ru != -1)
+                {
+                    int offset = abc_ru.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48);
+                    if (offset >= abc_ru.Length)
+                        offset = offset - abc_ru.Length;
+                    else if (offset < 0)
+                        offset = abc_ru.Length + offset;
+                    result += abc_ru[offset];
+                }
+                if (indexof_A_ru != -1)
+                {
+                    int offset = ABC_ru.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48);
+                    if (offset >= ABC_ru.Length)
+                        offset = offset - ABC_ru.Length;
+                    else if (offset < 0)
+                        offset = ABC_ru.Length + offset;
+                    result += ABC_ru[offset];
+                }
+                if (indexof_a == -1 && indexof_A == -1 && indexof_a_ru == -1 && indexof_A_ru == -1&&indexof_num == -1)
+                    result += text[i];
+
+            }
+            return result;
+        }
+
+        public string Decoding(string text)
+        {
+            string result = "";
+            PromptKey = Key;
+            while (PromptKey.Length < text.Length)
+            {
+                PromptKey += Key;
+            }
+            if (PromptKey.Length > text.Length)
+            {
+                int lenTxt = PromptKey.Length - text.Length;
+                for (int i = 0; i < lenTxt; i++)
+                {
+                    PromptKey += Key[i];
+                }
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                int indexof_a = abc.IndexOf(text[i]);
+                int indexof_A = ABC.IndexOf(text[i]);
+                int indexof_a_ru = abc_ru.IndexOf(text[i]);
+                int indexof_A_ru = ABC_ru.IndexOf(text[i]);
+                int indexof_num = num.IndexOf(text[i]);
+                if (indexof_num != -1)
+                {
+                    int offset = num.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48);
+                    if (offset >= num.Length)
+                        offset = offset - num.Length;
+                    else if (offset < 0)
+                        offset = num.Length + offset;
+                    result += num[offset];
+                }
+                if (indexof_a != -1)
+                {
+                    int offset = abc.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48)*-1; // вычитаем 48 для того чтобы перевести в число
+                    if (offset >= abc.Length)
+                        offset = offset - abc.Length;
+                    else if (offset < 0)
+                        offset = abc.Length + offset;
+                    result += abc[offset];
+                }
+                if (indexof_A != -1)
+                {
+                    int offset = ABC.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48)*-1;
+                    if (offset >= ABC.Length)
+                        offset = offset - ABC.Length;
+                    else if (offset < 0)
+                        offset = ABC.Length + offset;
+                    result += ABC[offset];
+                }
+                if (indexof_a_ru != -1)
+                {
+                    int offset = abc_ru.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48)*-1;
+                    if (offset >= abc_ru.Length)
+                        offset = offset - abc_ru.Length;
+                    else if (offset < 0)
+                        offset = abc_ru.Length + offset;
+                    result += abc_ru[offset];
+                }
+                if (indexof_A_ru != -1)
+                {
+                    int offset = ABC_ru.IndexOf(text[i]) + (Convert.ToInt32(PromptKey[i]) - 48)*-1;
+                    if (offset >= ABC_ru.Length)
+                        offset = offset - ABC_ru.Length;
+                    else if (offset < 0)
+                        offset = ABC_ru.Length + offset;
+                    result += ABC_ru[offset];
+                }
+                if (indexof_a == -1 && indexof_A == -1 && indexof_a_ru == -1 && indexof_A_ru == -1 && indexof_num == -1)
+                    result += text[i];
+
+            }
+            return result;
+        }
+    }
+}
